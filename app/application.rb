9a78b3f1e6b55bfe297765e5de139dca4af9499b
Chris Philbin <chris@chrisphilbin.net>
2019-02-27 00:46:51 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -23,7 +23,9 @@ class Application
         end
       end
     elsif
-      item_to_add = 
+      item_to_add = req.params(item)
+      if @@items.includes?(item_to_add)
+        @@cart << item_to add
     else
       resp.write "Path Not Found"
     end
