f91caae36497789afe75fc61177b9281726b7cc4
Chris Philbin <chris@chrisphilbin.net>
2019-02-27 00:51:48 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -22,7 +22,7 @@ class Application
           resp.write "#{item}\n"
         end
       end
-    elsif
+    elsif req.path.match(/)
       item_to_add = req.params["item"]
       if @@items.include? item_to_add
         @@cart << item_to add
